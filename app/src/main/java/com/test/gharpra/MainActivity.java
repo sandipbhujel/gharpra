566c7a0af0098f39212f70a3252eaa40fb67d8e3
Sandip Bhujel <bhujelsandip34@gmail.com>
2021-03-18 17:07:19 +0545

    update

--- app/src/main/java/com/test/gharpra/MainActivity.java
+++ app/src/main/java/com/test/gharpra/MainActivity.java
@@ -3,6 +3,9 @@ package com.test.gharpra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onBtnClick(View view){
+        TextView txtHello = findViewById(R.id.txtDisplay);
+        EditText txtName = findViewById(R.id.txtName);
+        txtHello.setText("Hello ,"+txtName.getText().toString()+ " Welcome");
     }
 }
